b328b068e96263031a050762ef18988df21a1be4
istiakAHMEDsaad <istik2020@gmail.com>
2025-04-03 18:28:04 +0600

    added features items

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 'use client';
+import FeaturedProduct from '@/components/FeaturedProduct';
 import Footer from '@/components/Footer';
 import HeaderSlider from '@/components/HeaderSlider';
 import HomeProducts from '@/components/HomeProducts';
@@ -10,7 +11,8 @@ export default function Home() {
       <Navbar />
       <div className='px-6 md:px-16 lg:px-32'>
         <HeaderSlider />
-        <HomeProducts/>
+        <HomeProducts />
+        <FeaturedProduct />
       </div>
       <Footer />
     </>
