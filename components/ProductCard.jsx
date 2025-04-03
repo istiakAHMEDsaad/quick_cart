a57bdc71cd5f71d621dc69e00a2c996497125376
istiakAHMEDsaad <istik2020@gmail.com>
2025-04-03 18:14:07 +0600

    home products

---
+++ components/ProductCard.jsx
@@ -0,0 +1,63 @@
+import { useAppContext } from '@/context/AppContext';
+import { assets } from '@/assets/assets';
+import Image from 'next/image';
+
+const ProductCard = ({ product }) => {
+  const { currency, router } = useAppContext();
+  return (
+    <div
+      className='flex flex-col items-start gap-0.5 max-w-[200px] w-full cursor-pointer'
+      onClick={() => {
+        router.push('/product' + product._id);
+        scrollTo(0, 0);
+      }}
+    >
+      {/* Image container */}
+      <div className='cursor-pointer group relative bg-gray-500/10 rounded-lg w-full h-52 flex items-center justify-center'>
+        <Image
+          src={product?.image[0]}
+          alt={product?.name}
+          className='group-hover:scale-105 transition object-cover w-4/5 h-4/5 md:w-full md:h-full'
+          width={800}
+          height={800}
+        />
+        <button className='absolute top-2 right-2 bg-white p-2 rounded-full shadow-md'>
+          <Image
+            className='h-3 w-3'
+            src={assets?.heart_icon}
+            alt='heart_icon'
+          />
+        </button>
+      </div>
+
+      {/* Product name */}
+      <p className='md:text-base text-center font-medium pt-2 w-full truncate'>
+        {product?.name}
+      </p>
+
+      {/* Product description */}
+      <p className='w-full text-xs text-gray-500/70 max-sm:hidden truncate'>
+        {product?.description}
+      </p>
+
+      {/* Product rating */}
+      <div className='flex items-center gap-2'>
+        <p className='text-xs'>{4.5}</p>
+        <div className='flex items-center gap-0.5'>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <Image
+              key={index}
+              className='h-3 w-3'
+              src={
+                index < Math.floor(4) ? assets.star_icon : assets.star_dull_icon
+              }
+              alt='star_icon'
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
